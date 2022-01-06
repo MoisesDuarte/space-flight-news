fba09bafa4ab3258dc362331c09f68ecbf813557
MoisesDuarte <moisesduartejunior@gmail.com>
2022-01-06 20:15:21 -0300

    Documentando rotas com swagger

--- server/api/routes/ArticleRoutes.js
+++ server/api/routes/ArticleRoutes.js
@@ -3,8 +3,126 @@ const router = express.Router();
 
 const articleController = require("../controllers/ArticleController");
 
+// TODO: Maybe define these schemas on a separate file
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Article:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: "Mongo auto-generated id"
+ *           example: "string"
+ *         id:
+ *           type: string
+ *           example: "string"
+ *         featured:
+ *           type: boolean
+ *           example: false
+ *         title:
+ *           type: string
+ *           example: "string"
+ *         url:
+ *           type: string
+ *           example: "string"
+ *         imageUrl:
+ *           type: string
+ *           example: "string"
+ *         newsSite:
+ *           type: string
+ *           example: "string"
+ *         summary:
+ *           type: string
+ *           example: "string"
+ *         publishedAt:
+ *           type: string
+ *           example: "string"
+ *         launches:
+ *           type: array
+ *           items:
+ *             type: object
+ *             properties:
+ *               _id:
+ *                 type: string
+ *                 example: "string"
+ *               provider:
+ *                 type: string
+ *                 example: "string"
+ *     ArticleList:
+ *       type: object
+ *       properties:
+ *         articles:
+ *           type: array
+ *           items:
+ *             oneOf:
+ *               - $ref: "#/components/schemas/Article"
+ *         page:
+ *           type: integer
+ *           example: 0
+ *         pages:
+ *           type: integer
+ *           example: 0
+ */
+
 // * API Routes
+/**
+ * @swagger
+ * /articles:
+ *   get:
+ *     summary: Retrieve a list of news articles
+ *     description: Retrieves a list of news articles. Can be paginated and queried by title for blog listings.
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: page of query to get
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The number of articles to return
+ *       - in: query
+ *         name: title
+ *         schema:
+ *           type: string
+ *         required: false
+ *         description: Filter search by title
+ *     responses:
+ *       200:
+ *         description: A list of articles.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/ArticleList"
+ */
 router.get("/", articleController.getAllArticles);
+
+/**
+ * @swagger
+ * /articles/{id}:
+ *   get:
+ *     summary: Retrieve a single article
+ *     description: Retrieve a single article. Can be used to show details of current article
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The article id
+ *     responses:
+ *       200:
+ *         description: A single article
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Article"
+ */
 router.get("/:id", articleController.getArticleById);
 
 module.exports = router;
--- server/app.js
+++ server/app.js
@@ -2,7 +2,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 require("dotenv").config();
+
+// ? Constants
+const PORT = process.env.PORT || 3000;
+
+// ? Swagger Definitions
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Space Flight News Mongo API",
+      version: "1.0.0",
+      description:
+        "This is a REST API application made with Express and Mongoose. It retrieves data from a database with data served by the official Space Flight News API",
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+        description: "Development Server",
+      },
+    ],
+  },
+  apis: ["./api/routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 // ? Cron Jobs
 require("./jobs/updater");
@@ -28,10 +55,10 @@ app.get("/", (req, res) => {
   res.status(200).json("Fullstack Challenge 2021 🏅 - Space Flight News");
 });
 
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/articles", articleRoutes);
 
 // ? Server listening
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
 });
