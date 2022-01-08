44171b85bfdece29672fd4d7554fa25792f6521d
MoisesDuarte <moisesduartejunior@gmail.com>
2022-01-07 21:13:23 -0300

    Client: Testando listagem inicial de artigos

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,7 +5,7 @@
       <h1 class="py-6 is-size-3">Space Flight News</h1>
       <hr />
 
-      <ArticleList />
+      <ArticleList :articles="articles" />
 
       <section>
         <button class="button is-link is-outlined">Carregar mais</button>
@@ -17,12 +17,23 @@
 <script>
 import AppNavbar from "./components/base/AppNavbar.vue";
 import ArticleList from "./components/ArticleList.vue";
+import ApiResource from "./resources/ApiResource";
 
 export default {
   name: "App",
   components: {
     AppNavbar,
     ArticleList,
+  },
+  data() {
+    return {
+      articles: [],
+    };
+  },
+  created() {
+    ApiResource.getArticles(0, 10).then((res) => {
+      this.articles = res.articles;
+    });
   },
 };
 </script>
--- client/src/components/ArticleList.vue
+++ client/src/components/ArticleList.vue
@@ -2,7 +2,13 @@
   <section class="article-list mb-4">
     <div class="columns">
       <div class="column is-three-fifths is-offset-one-fifth">
-        <ArticleCard />
+        <ArticleCard
+          v-for="(article, index) in articles"
+          :key="article._id"
+          :information="article"
+          :class="{ 'is-flex-direction-row-reverse': index & 1 }"
+          class="mb-6"
+        />
       </div>
     </div>
   </section>
@@ -13,6 +19,12 @@ import ArticleCard from "./ArticleCard.vue";
 
 export default {
   name: "ArticleList",
+  props: {
+    articles: {
+      type: Array,
+      default: [],
+    },
+  },
   components: {
     ArticleCard,
   },
