21fd5a597a2322ffdb4abf1d2f5351efef49af01
MoisesDuarte <moisesduartejunior@gmail.com>
2022-01-07 21:48:13 -0300

    Client: Lógica para fetch com base em campo de pesquisa e expansão de lista de artigos visiveis

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <main>
-    <AppNavbar />
+    <AppNavbar @onSearch="sortByTitle($event)" />
     <div class="container has-text-centered">
       <h1 class="py-6 is-size-3">Space Flight News</h1>
       <hr />
 
-      <ArticleList :articles="articles" />
+      <ArticleList v-show="articles.length > 0" :articles="articles" />
 
       <section>
-        <button class="button is-link is-outlined">Carregar mais</button>
+        <button
+          v-if="pagination.totalPages - 1 > pagination.page"
+          class="button is-link is-outlined mb-6"
+          :disabled="isLoading"
+          @click="loadMoreArticles"
+        >
+          Carregar mais
+        </button>
       </section>
     </div>
   </main>
@@ -28,12 +35,37 @@ export default {
   data() {
     return {
       articles: [],
+      pagination: {
+        title: "",
+        page: 0,
+        totalPages: 10,
+      },
+      isLoading: false,
     };
   },
+  methods: {
+    fetchArticles(page, limit, title = null, expand = false) {
+      this.isLoading = true;
+
+      ApiResource.getArticles(page, limit, title)
+        .then(({ articles, pagination }) => {
+          this.articles = expand ? [...this.articles, ...articles] : articles;
+          this.pagination = pagination;
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    },
+    sortByTitle(title) {
+      this.fetchArticles(0, 10, title);
+    },
+    loadMoreArticles() {
+      const { page, title } = this.pagination;
+      this.fetchArticles(page + 1, 10, title, true);
+    },
+  },
   created() {
-    ApiResource.getArticles(0, 10).then((res) => {
-      this.articles = res.articles;
-    });
+    this.fetchArticles(0, 10);
   },
 };
 </script>
--- client/src/components/base/AppNavbar.vue
+++ client/src/components/base/AppNavbar.vue
@@ -3,7 +3,22 @@
     <div class="navbar-menu is-active">
       <div class="navbar-end">
         <div class="navbar-item is-flex-mobile">
-          <input class="input mr-2" type="text" placeholder="Search" />
+          <!-- ? Search input -->
+          <div class="field has-addons mb-0 mr-2">
+            <div class="control">
+              <input
+                v-model="searchInput"
+                class="input"
+                type="text"
+                placeholder="Search"
+                @keydown.native.enter="onSearch"
+              />
+            </div>
+            <div class="control">
+              <a role="button" class="button is-info" @click="onSearch"> L </a>
+            </div>
+          </div>
+
           <div class="select">
             <select>
               <option>Mais antigas</option>
@@ -19,5 +34,15 @@
 <script>
 export default {
   name: "AppNavbar",
+  data() {
+    return {
+      searchString: "",
+    };
+  },
+  methods: {
+    onSearch() {
+      this.$emit("onSearch", this.searchInput);
+    },
+  },
 };
 </script>
