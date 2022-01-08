1300d2a7bfd2964a7108028155d94c1c3f663381
MoisesDuarte <moisesduartejunior@gmail.com>
2022-01-08 19:08:40 -0300

    Client: Melhorando feedback de ações de admin

--- client/src/views/Admin.vue
+++ client/src/views/Admin.vue
@@ -9,6 +9,16 @@
   </AppModal>
 
   <div class="container py-6">
+    <transition name="fade">
+      <div
+        v-if="isNotificationVisible"
+        class="notification"
+        :class="notification.type"
+      >
+        {{ notification.text }}
+      </div>
+    </transition>
+
     <section class="is-flex mb-4 is-justify-content-space-between">
       <div class="buttons">
         <button class="button" @click="isModalActive = !isModalActive">
@@ -75,6 +85,8 @@ export default {
       currentArticle: {},
       isModalActive: false,
       isEditMode: false,
+      isNotificationVisible: false,
+      notification: "",
     };
   },
   methods: {
@@ -89,11 +101,14 @@ export default {
     },
     createArticle(data) {
       ApiResource.addNewArticle(data)
-        .then((res) => {
-          alert("Created Article:", JSON.stringify(res));
+        .then(() => {
+          this.triggerNotification(
+            "Novo artigo cadastrado com sucesso!",
+            "is-success"
+          );
         })
-        .catch((err) => {
-          alert(err);
+        .catch(() => {
+          this.triggerNotification("Erro ao cadastrar artigo!", "is-danger");
         })
         .finally(() => {
           this.isModalActive = false;
@@ -101,11 +116,11 @@ export default {
     },
     editArticle(data) {
       ApiResource.updateArticle(data._id, data)
-        .then((res) => {
-          alert(`Update efetuado com sucesso!`);
+        .then(() => {
+          this.triggerNotification("Artigo editado com sucesso!", "is-success");
         })
-        .catch((err) => {
-          alert(err);
+        .catch(() => {
+          this.triggerNotification("Erro ao editar artigo!", "is-danger");
         })
         .finally(() => {
           this.currentArticle = {};
@@ -114,6 +129,20 @@ export default {
 
           const { page, title } = this.pagination;
           this.fetchArticles(page, 10, title);
+        });
+    },
+    deleteArticle(id) {
+      ApiResource.deleteArticle(id)
+        .then(() => {
+          this.fetchArticles(0, 10);
+          this.triggerNotification(
+            "Remoção efetuada com sucesso!",
+            "is-success"
+          );
+        })
+        .catch((err) => {
+          console.info(err);
+          this.triggerNotification("Erro ao remover artigo!", "is-danger");
         });
     },
     openEditModal(article) {
@@ -125,16 +154,19 @@ export default {
       this.isEditMode = true;
       this.isModalActive = true;
     },
-    deleteArticle(id) {
-      ApiResource.deleteArticle(id).then(() => {
-        this.fetchArticles(0, 10);
-      });
-    },
     onSearch() {
       this.fetchArticles(0, 10, this.searchString);
     },
     goToPage(page) {
       this.fetchArticles(page, 10);
+    },
+    triggerNotification(text, type) {
+      this.notification = { text, type };
+      this.isNotificationVisible = true;
+
+      setTimeout(() => {
+        this.isNotificationVisible = false;
+      }, 3000);
     },
   },
   created() {
